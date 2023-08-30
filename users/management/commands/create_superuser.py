c9d69824f8f9f327b7b65a3fed9dd168e1db2ffb
Tanzhor_RIK <tanzhor4@gmail.com>
2023-08-31 02:37:36 +0700

    Added versatility when creating

--- users/management/commands/create_superuser.py
+++ users/management/commands/create_superuser.py
@@ -6,11 +6,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email = 'admin@admin.ru',
+            email = input("Адрес почты:"),
             first_name = 'Admin',
             last_name = 'Admin',
             is_staff = True,
             is_superuser = True
+
         )
-        user.set_password('kaliningrad39')  #установка пароля
+        user.set_password(input('Пароль:'))  #установка пароля
         user.save()  # сохраниние в БД
